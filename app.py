19be7417a92ba582e317a134948c3aea9daeca60
Andressa-Montanini <142947147+Andressa-Montanini@users.noreply.github.com>
2025-03-04 00:27:06 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, send_from_directory
+import mysql.connector
+
+app = Flask(__name__)
+
+# Conexão com o banco de dados
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='Buch_159753',
+        database='gerenciador_livros'
+    )
+    return conn
+
+# Rota para servir o HTML
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+# Funcionalidade 1: Adicionar Livro
+@app.route('/adicionar_livro', methods=['POST'])
+def adicionar_livro():
+    titulo = request.json['titulo']
+    autor = request.json['autor']
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO livros (titulo, autor) VALUES (%s, %s)', (titulo, autor))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({'message': 'Livro adicionado com sucesso!'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
