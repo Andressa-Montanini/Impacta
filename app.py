e2bda013153846bdcdcb640c5acb8bcdd68ebe18
Andressa-Montanini <142947147+Andressa-Montanini@users.noreply.github.com>
2025-04-03 20:37:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='Andressa',
+        password='123456789',
         database='gerenciador_livros'
     )
     return conn
@@ -30,6 +30,17 @@ def adicionar_livro():
     cursor.close()
     conn.close()
     return jsonify({'message': 'Livro adicionado com sucesso!'}), 201
+
+# Funcionalidade 2: Listar Livros
+@app.route('/listar_livros', methods=['GET'])
+def listar_livros():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livros')
+    livros = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(livros), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
